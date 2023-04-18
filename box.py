e50af6a9a1e1483d7b203924ea25b2d06b30fb50
chew calvin <62589459+calvin19999@users.noreply.github.com>
2023-04-18 22:05:27 +0800

    Update box.py

--- box.py
+++ box.py
@@ -1,16 +1,25 @@
 import bpy
 
-# Create a new cube mesh
-mesh = bpy.data.meshes.new("BoxMesh")
-cube_object = bpy.data.objects.new("BoxObject", mesh)
+# Define the dimensions of the box
+width = 2
+height = 2
+depth = 2
 
-# Add the object to the scene
-bpy.context.scene.collection.objects.link(cube_object)
+# Create a new mesh object
+mesh = bpy.data.meshes.new("Box")
 
-# Define the vertices and faces of the mesh
-verts = [(1, 1, 1), (1, -1, 1), (-1, -1, 1), (-1, 1, 1), (1, 1, -1), (1, -1, -1), (-1, -1, -1), (-1, 1, -1)]
-faces = [(0, 1, 2, 3), (0, 4, 5, 1), (1, 5, 6, 2), (2, 6, 7, 3), (3, 7, 4, 0), (4, 7, 6, 5)]
+# Create a list of vertices for the box
+verts = [(0, 0, 0), (0, height, 0), (width, height, 0), (width, 0, 0),
+         (0, 0, depth), (0, height, depth), (width, height, depth), (width, 0, depth)]
+
+# Create a list of faces for the box
+faces = [(0, 1, 2, 3), (4, 5, 6, 7), (0, 4, 5, 1), (1, 5, 6, 2), (2, 6, 7, 3), (3, 7, 4, 0)]
 
 # Add the vertices and faces to the mesh
 mesh.from_pydata(verts, [], faces)
-mesh.update()
+
+# Create a new object for the mesh
+obj = bpy.data.objects.new("Box", mesh)
+
+# Add the object to the scene
+bpy.context.scene.collection.objects.link(obj)
